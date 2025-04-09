43a7447b88844230431b463e29d80ce5318ab4b4
lamarquenet <35860662+lamarquenet@users.noreply.github.com>
2025-04-08 23:58:46 -0300

    Delete dashboard-app directory

--- dashboard-app/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* other config options can go here */
-};
-
-export default nextConfig;
--- dashboard-app/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export interface Site {
-  id: string;
-  name: string;
-  link: string;
-  description: string;
-  thumbnailUrl?: string; // Optional property
-}
